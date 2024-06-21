bd9a82496ceb112628733c6fb7cefe509f7026b9
nvkaa <khoa.nguyenviet.549@gmail.com>
2024-06-21 17:27:45 +0700

    countdown clock running now

--- countdown/script.js
+++ countdown/script.js
@@ -1,22 +1,34 @@
 
-const now = new Date()
-const ny = new Date(2025, 1, 1)
+const getTimeLeft = () => {
+    const now = new Date()
+    const ny = new Date(2025, 1, 1)
 
-const dif = ny.getTime() - now.getTime()
+    const dif = ny.getTime() - now.getTime()
 
-const daysLeft = Math.floor(dif / (1000 * 60 * 60 * 24))
-const hoursLeft = Math.floor((dif % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-const minutesLeft = Math.floor((dif % (1000 * 60 * 60)) / (1000 * 60))
-const secondsLeft = Math.floor((dif % (1000 * 60)) / 1000)
+    const daysLeft = Math.floor(dif / (1000 * 60 * 60 * 24))
+    const hoursLeft = Math.floor((dif % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    const minutesLeft = Math.floor((dif % (1000 * 60 * 60)) / (1000 * 60))
+    const secondsLeft = Math.floor((dif % (1000 * 60)) / 1000)
+
+    return {
+        daysLeft, hoursLeft, minutesLeft, secondsLeft
+    }
+}
+
+
 
 const d = document.querySelector('[days]')
 const h = document.querySelector('[hours]')
 const m = document.querySelector('[minutes]')
 const s = document.querySelector('[seconds]')
 
-console.log(daysLeft, d);
+const displayTime = () => {
+    let timeleft = getTimeLeft() 
 
-d.textContent = daysLeft
-h.textContent = hoursLeft
-m.textContent = minutesLeft
-s.textContent = secondsLeft
+    d.textContent = timeleft.daysLeft
+    h.textContent = timeleft.hoursLeft
+    m.textContent = timeleft.minutesLeft
+    s.textContent = timeleft.secondsLeft
+}
+
+setInterval(displayTime, 1000)
